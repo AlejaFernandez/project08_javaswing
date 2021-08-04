4d12d595a88a27f484db31af74ea045e9777cd3a
Alejandra Fernandez <alejajaf.g@mail.com>
2021-08-04 07:28:21 -0500

    commit final calculadora de dolares

--- src/main/java/com/misiontic/project08_swing_02/MainWindow.java
+++ src/main/java/com/misiontic/project08_swing_02/MainWindow.java
@@ -8,8 +8,12 @@ package com.misiontic.project08_swing_02;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -17,9 +21,13 @@ import javax.swing.JTextField;
  *
  * @author aleja
  */
-public class MainWindow extends JFrame {
-    
+public class MainWindow extends JFrame implements ActionListener{
+    private JLabel description, result;
+    private JTextField inputDolar;
+    private JButton calculate;
     MainWindow(){
+        
+        
         //setBounds(450,200,500,200);//tamaño y posicion
         //setSize(300,300);
         //setLocationRelativeTo(null);//centrar
@@ -34,15 +42,27 @@ public class MainWindow extends JFrame {
         JPanel panel =new JPanel();
         panel.setLayout(null); //desactivar layaot predeterminado
         this.getContentPane().add(panel); //El panel se encarga de ubicar los componentes
-        JLabel description = new JLabel("Convertir de dolares a pesos");
+        description = new JLabel("Convertir de dolares a pesos");
         description.setBounds(10, 10, 50, 50);
         panel.add(description); //Agregar elemento al panel dentro de la ventana
-        JTextField inputDolar = new JTextField();//Crear casilla
+         inputDolar = new JTextField();//Crear casilla
         inputDolar.setBounds(10,60,100,30);//dimensiones del  
         panel.add(inputDolar);//Agregar elemento 
-        JLabel result= new JLabel("0,0");//mostar resultado con un texto
+         result= new JLabel("0,0");//mostar resultado con un texto
         result.setBounds(30,100,100,30);//dimensiones del texto
         panel.add(result);//agreagar al panel
+        JButton calculateButton =new JButton("Calcular");//crear boton
+        calculateButton.setBounds(10,140,100,30);//dimensiones del boton
+        calculateButton.addActionListener(this);//implementar interfaz para evento del button
+        panel.add(calculateButton);//agregar boton al panel
     
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String dolar= inputDolar.getText();
+        double pesos =Double.parseDouble(dolar)*3900;
+        result.setText(""+pesos);
+        JOptionPane.showMessageDialog(this,"ok");
     }
 }
